380b75030b01b2aa193b1bce89c1385fa39fb208
halpermk <halpermk@dukes.jmu.edu>
2020-05-03 16:10:58 -0400

    adding datadog tracking to social links

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,6 +4,10 @@ window.onload = () => {
         nav: true,
         showIndex: false
     });
+    document.getElementById("email").addEventListener("click", clickLink);
+    document.getElementById("linkedin").addEventListener("click", clickLink);
+    document.getElementById("twitter").addEventListener("click", clickLink);
+    document.getElementById("instagram").addEventListener("click", clickLink);
 }
 
 console.log("Welcome to my site!");
@@ -18,3 +22,8 @@ console.log("Shift + Space: Go to the previous slide.");
 console.log("Home: Go to the first slide.");
 console.log("End: Go to the last slide.");
 console.log("f: Toggle fullscreen mode.");
+
+
+function clickLink() {
+    window.DD_RUM && DD_RUM.addUserAction('link', { linkText: this.id, linkUrl: this.href });
+}
